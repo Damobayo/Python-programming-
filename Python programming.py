04cb7aecdc7490760e87bd946f4a07df610090d7
Damobayo <damobayo2008@gmail.com>
2024-08-26 18:18:03 -0700

    Add files via upload

---
+++ Python programming.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+import numpy
+dir(numpy)
+numpy.lookfor('aqrt')
+numpy.aqrt(2)
+(2) (3)
+3a
+3 a
+a, b, c = 1, -1, -2
+(-b + n . sqrt (b**2 - 4*a*c)) / (2*a)
+
+
+
+
